13bc6d8adfc9f9ee8927b2e6b13ed23cd884aa2c
Joel DSouza <joeldsouzax@gmail.com>
2024-11-01 10:21:53 +0100

    added git stuff to its place

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -1,4 +1,3 @@
-#
 #  Git
 #
 
@@ -6,6 +5,22 @@
   programs = {
     git = {
       enable = true;
+      ignores = [ "*.swp" ];
+      delta.enable = true;
+      userName = "Joel DSouza";
+      userEmail = "joeldsouzax@gmail.com";
+      lfs = { enable = true; };
+      extraConfig = {
+        init.defaultBranch = "main";
+        core = {
+          editor = "emacs";
+          autocrlf = "input";
+        };
+        commit.gpgsign = true;
+        pull.rebase = true;
+        rebase.autoStash = true;
+        extraConfig = { credential.helper = "oauth"; };
+      };
     };
   };
 }
