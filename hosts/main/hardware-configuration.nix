6de18a85665c672680b539249ab68a3fbf0eda35
Joel DSouza <joeldsouzax@gmail.com>
2024-11-04 20:55:08 +0100

    added tx20u driver

--- hosts/main/hardware-configuration.nix
+++ hosts/main/hardware-configuration.nix
@@ -10,7 +10,7 @@
     [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.rtl8192eu ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.rtl8852au ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/5f48eeb3-2368-4f34-95ed-bb919296d841";
@@ -28,10 +28,11 @@
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
+  #
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking = with host; {
-    useDHCP = lib.mkDefault true;
+    useDHCP = lib.mkDefault false;
     hostName = hostname;
     networkmanager = { enable = true; };
     firewall.allowedTCPPorts = [ 57621 ];
