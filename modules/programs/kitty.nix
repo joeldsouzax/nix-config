fec8c0f51884ac0ad446cdc720291c11b242d7e5
Joel DSouza <joeldsouzax@gmail.com>
2024-11-07 11:26:44 +0100

    added kitty keybindings

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -1,24 +1,23 @@
-#
-#  Terminal Emulator
+# Terminal Emulator
 #
 
 { pkgs, vars, ... }:
 
-let
-  colors = import ../theming/colors.nix;
-in
-{
-  environment = {
-    systemPackages = with pkgs; [
-      kitty
-    ];
-  };
+let colors = import ../theming/colors.nix;
+in {
+  environment = { systemPackages = with pkgs; [ kitty ]; };
 
   home-manager.users.${vars.user} = {
     programs = {
       kitty = {
         enable = true;
         themeFile = "Afterglow";
+        keybindings = {
+          "ctrl+1" = "goto_tab 1";
+          "ctrl+2" = "goto_tab 2";
+          "ctrl+3" = "goto_tab 3";
+          "ctrl+4" = "goto_tab 4";
+        };
         settings = {
           confirm_os_window_close = 0;
           enable_audio_bell = "no";
