17bbe9107971fe67b73d8ec0f02f30d991c9a5dc
Joel DSouza <joeldsouzax@gmail.com>
2025-02-11 20:19:56 +0100

    updated config

--- modules/editors/doom-emacs/alt/doom.sh
+++ modules/editors/doom-emacs/alt/doom.sh
@@ -4,8 +4,8 @@ DOOM="$HOME/.emacs.d"
 
 if [ ! -d "$DOOM" ]; then
   git clone https://github.com/hlissner/doom-emacs.git $DOOM
-  alacritty -e $DOOM/bin/doom -y install & disown
+  kitty -e $DOOM/bin/doom -y install & disown
 else
-  alacritty -e $DOOM/bin/doom sync
+  kitty -e $DOOM/bin/doom sync
 fi
 
