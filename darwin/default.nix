7dbb800cf3d35a23f686616623c76670198c3f57
Joel DSouza <joeldsouzax@gmail.com>
2025-03-29 22:10:41 +0100

    feat: removed homebrew alltogether

--- darwin/default.nix
+++ darwin/default.nix
@@ -12,29 +12,29 @@
           users.joeldsouza.imports = [ ./modules/home-manager ];
         };
       }
-      nix-homebrew.darwinModules.nix-homebrew
-      {
-        nix-homebrew = {
-          # Install Homebrew under the default prefix
-          enable = true;
-          # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-          enableRosetta = true;
-          # User owning the Homebrew prefix
-          user = "joeldsouza";
-          # Optional: Declarative tap management
-          # taps = {
-          #   "homebrew/homebrew-core" = homebrew-core;
-          #   "homebrew/homebrew-cask" = homebrew-cask;
-          #   "homebrew/homebrew-bundle" = homebrew-bundle;
-          #   "d12frosted/homebrew-emacs-plus" = emacs-plus;
-          # };
+      # nix-homebrew.darwinModules.nix-homebrew
+      # {
+      #   nix-homebrew = {
+      #     # Install Homebrew under the default prefix
+      #     enable = true;
+      #     # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+      #     enableRosetta = true;
+      #     # User owning the Homebrew prefix
+      #     user = "joeldsouza";
+      #     # Optional: Declarative tap management
+      #     # taps = {
+      #     #   "homebrew/homebrew-core" = homebrew-core;
+      #     #   "homebrew/homebrew-cask" = homebrew-cask;
+      #     #   "homebrew/homebrew-bundle" = homebrew-bundle;
+      #     #   "d12frosted/homebrew-emacs-plus" = emacs-plus;
+      #     # };
 
-          # Optional: Enable fully-declarative tap management
-          #
-          # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-          mutableTaps = false;
-        };
-      }
+      #     # Optional: Enable fully-declarative tap management
+      #     #
+      #     # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+      #     mutableTaps = false;
+      #   };
+      # }
     ];
 
     darwinPackages = self.darwinConfigurations."joel".pkgs;
