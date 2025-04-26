9883966e6807cf35942ae81e4bd76b9418630a65
Joel DSouza <joeldsouzax@gmail.com>
2025-04-26 23:03:51 +0200

    feat: added calendar

--- modules/programs/accounts.nix
+++ modules/programs/accounts.nix
@@ -1,4 +1,3 @@
-#
 # Gnome Control Manager
 # Mainly used for online accounts
 #
@@ -18,8 +17,25 @@
 
     home-manager.users.${vars.user} = {
       xdg.desktopEntries.gnome-control-center = {
-        name = "Gnome Control Center";
-        exec = "env XDG_CURRENT_DESKTOP=GNOME ${pkgs.gnome-control-center}/bin/gnome-control-center";
+        name = "Control";
+        exec =
+          "env XDG_CURRENT_DESKTOP=GNOME ${pkgs.gnome-control-center}/bin/gnome-control-center";
+      };
+
+      programs = {
+        accounts = {
+          calendar = {
+            ## TODO: complete calendar and email
+            basePath = "Calendar";
+            accounts.personal_gmail = {
+              remote.type = "google_calendar";
+              vdirsyncer = {
+                enable = true;
+                collections = [ "from a" ];
+              };
+            };
+          };
+        };
       };
     };
   };
